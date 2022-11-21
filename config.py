d2e5b652214fa0042ab3397866e4a0bb35432032
Punam2804 <Punam2804>
2022-11-21 15:04:09 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 5505
+
+Pickle_file = r"E:\Practies\Machine_Learning\Logistic\Loan_Data\models\Loanpkl.pkl"
+
+json_file = r"E:\Practies\Machine_Learning\Logistic\Loan_Data\models\json_data.json"
